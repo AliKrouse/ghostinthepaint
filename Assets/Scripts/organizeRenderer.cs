123571dc6c43b064cd7275ae26d67678c0fe1d69
Ali <alice.krouse@gmail.com>
2018-11-26 15:58:10 -0600

    added environment

---
+++ Assets/Scripts/organizeRenderer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class organizeRenderer : MonoBehaviour
+{
+    protected SpriteRenderer sr;
+    
+	protected virtual void Start ()
+    {
+        sr = GetComponent<SpriteRenderer>();
+	}
+
+    protected virtual void Organize()
+    {
+        sr.sortingOrder = Mathf.RoundToInt(transform.position.y * -100);
+    }
+}
